8d0e60755e4a01ec5fd82af7c65aa3bd671a7b08
Antosser <anton.moonalien@gmail.com>
2021-08-30 20:07:19 +0200

    Modified main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,8 @@
 #include <vector>
 #include <thread>
 
-void trd(std::vector<char>& vect);
-
 int main(int argc, char* argv[]) {
-	std::vector<char> mem;
-	std::thread t1(trd, std::ref(mem));
-
-	std::cout << "Max size: " << (float)mem.max_size() / 1000000000 << " gigabytes.\n";
-
 	while (true) {
-			mem.push_back(0x00);
-	}
-
-	
-	//std::cin.get();
-}
-
-void trd(std::vector<char>& vect) {
-	while (true) {
-		std::cout << "Allocated " << (float)vect.size() / 1000000000 << " gigabytes.\n";
-		std::this_thread::sleep_for(std::chrono::milliseconds(250));
+		malloc(0x100000);
 	}
 }
