26ea82baf57bb7fbbd4ab518254b9ebcdeaa7e9e
Antosser <anton.moonalien@gmail.com>
2021-09-18 13:33:55 +0200

    Added normal mode

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <corecrt_malloc.h>
+#include <vector>
 
-int main() {
+int main(int argc, const char** argv) {
+	if (argc < 2 || argv[1][0] != 'n')
 	while (true) {
 		malloc(0x100000);
 	}
+	std::vector<char> mem;
+	while (true)
+	mem.push_back(0x00);
 }
