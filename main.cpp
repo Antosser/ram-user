b10f8e52baf1c7131bf81ae513cfce94ef7b6044
Antosser <anton.moonalien@gmail.com>
2021-11-23 21:29:44 +0100

    One better mode

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <corecrt_malloc.h>
-#include <vector>
+#include <thread>
 
 int main(int argc, const char** argv) {
-	if (argc > 1 && argv[1][0] != 'h')
 	while (true) {
 		malloc(0x100000);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
-	std::vector<char> mem;
-	while (true)
-	mem.push_back(0x00);
 }
